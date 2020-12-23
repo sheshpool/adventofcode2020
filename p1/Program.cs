065ecc485496c0447ed220d516c8cbbcdd682b77
haddark <khaddar@scop-it.com>
2020-12-23 11:51:55 +0100

    Init, add projects

---
+++ p1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            const int TARGET_VALUE = 2020;
+
+            Console.WriteLine("Starting..");
+            var reader = new StreamReader(File.OpenRead(@"input.csv"));
+            List<int> expenseList = new List<int>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                expenseList.Add(Int32.Parse(line));
+            }
+            expenseList.Sort();
+            int i, j, k;
+            bool isFound = false;
+            int nbIt = 0;
+            i=0; j=0;
+            /* Partie 1*/
+            while(i<expenseList.Count && expenseList[i]<= TARGET_VALUE && !isFound)
+            {
+                j = 0;
+                while(j<expenseList.Count && expenseList[i] + expenseList[j] <= TARGET_VALUE && !isFound)
+                {
+                    if(expenseList[i] + expenseList[j] == TARGET_VALUE)
+                    {
+                        Console.WriteLine("{0} x {1} = {2}", expenseList[i], expenseList[j], expenseList[i]*expenseList[j]);
+                        isFound = true;
+                    }
+                    j++;
+                    nbIt++; //652/40000 combinaisons totales
+                }
+                i++;
+            }
+            Console.WriteLine(nbIt);
+            /* Partie 2*/
+            i=0; j=0;k=0;isFound=false;nbIt=0;
+            while(i<expenseList.Count && expenseList[i]<= TARGET_VALUE && !isFound)
+            {
+                j = 0;
+                while(j<expenseList.Count && expenseList[i] + expenseList[j] <= TARGET_VALUE && !isFound)
+                {
+                    k=0;
+                    while(k<expenseList.Count && expenseList[i] + expenseList[j] + expenseList[k]<= TARGET_VALUE && !isFound)
+                    {                   
+                        if(expenseList[i] + expenseList[j] + expenseList[k] == TARGET_VALUE)
+                        {
+                            Console.WriteLine("{0} x {1} x {2} = {3}", expenseList[i], expenseList[j], expenseList[k], expenseList[i]*expenseList[j]*expenseList[k]);
+                            isFound = true;
+                        }
+                        k++;
+                        nbIt++;
+                    }             
+                    j++;
+                    nbIt++; //447/8000000 combinaisons totales
+                }
+                i++;
+            }
+            Console.WriteLine("End.");
+            Console.WriteLine(nbIt);
+            Console.Read();
+        }
+    }
+}
---
+++ p2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting..");
+            List<string> pwdList = new List<string>();
+
+            var reader = new StreamReader(File.OpenRead(@"input.csv"));
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                pwdList.Add(line);
+            }
+
+            /* Partie 1*/
+
+            string pattern = @"-|: | ";
+            int nb =0;
+            foreach(string pwd in pwdList)
+            {
+                string[] substrings = Regex.Split(pwd, pattern);
+                int count = substrings[3].Count(c => c == substrings[2][0]);               
+                //Console.WriteLine("{0} {1} {2} {3} => {4}", substrings[0], substrings[1], substrings[2][0], substrings[3], count);
+                if(count >= Int32.Parse(substrings[0]) && count <= Int32.Parse(substrings[1]))
+                {
+                    nb++;
+                }
+            }
+            Console.WriteLine("Nb = {0}", nb);
+            /* Partie 2*/
+            nb =0;
+            foreach(string pwd in pwdList)
+            {
+                int i=0;
+                string[] substrings = Regex.Split(pwd, pattern);
+
+                //Console.WriteLine("{0} {1} {2} {3} => {4}", substrings[0], substrings[1], substrings[2][0], substrings[3], count);
+                i = substrings[3][Int32.Parse(substrings[0]) - 1] == substrings[2][0] ? i+1 : i;
+                i = substrings[3][Int32.Parse(substrings[1]) - 1] == substrings[2][0] ? i+1 : i;
+                nb = i == 1 ? nb + 1: nb;              
+            }
+            Console.WriteLine("Nb = {0}", nb);          
+            Console.WriteLine("End.");
+        }      
+    }
+}
