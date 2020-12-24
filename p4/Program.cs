03af462c60355da19d549f0cfb383ded6a3523fd
haddark <khaddar@scop-it.com>
2020-12-24 09:23:49 +0100

    Create p4 project

---
+++ p4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace p4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
